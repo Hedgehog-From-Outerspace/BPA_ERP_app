ca40dcabfb12e6eb3e0326b890727cdc0c8b1c74
Hedgehog-From-Outerspace <100069852+Hedgehog-From-Outerspace@users.noreply.github.com>
2024-06-26 07:35:06 +0200

    Setup of all basic pages, navbar setup, js + css setup

--- website/views.py
+++ website/views.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return '<h1>Hello, World!<h1>'
+    return render_template("home.html")
+
+@views.route('/klant')
+def customer():
+    return render_template("customer.html")
+
+@views.route('/accountmanager')
+def accountmanager():
+    return render_template("accountmanager.html")
+
+@views.route('/voorraadbeheer')
+def inventorykmanagement():
+    return render_template("inventorymanager.html")
+
+@views.route('/leverancier')
+def supplier():
+    return render_template("supplier.html")
+
+@views.route('/productie')
+def production():
+    return render_template("production.html")
